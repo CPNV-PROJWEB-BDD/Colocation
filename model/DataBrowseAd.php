46af7f27b9bd5647a8046fc0e0556b99f2525197
Jonathan-jony <Jonathan.PENARANDA-GONZALEZ@cpnv.ch>
2023-03-10 09:20:39 +0100

    Mise à jour d'un fichier

--- model/DataBrowseAd.php
+++ model/DataBrowseAd.php
@@ -6,40 +6,6 @@
  * @version 23.02.2023
  */
 
-$colocation = array(
-    array(
-        'Habitation' => 'Appartement',
-        'Localisation' => 'Yverdon',
-        'Pièces' => 3.5
-    ),
-    array(
-        'Habitation' => 'Appartement',
-        'Localisation' => 'Ste-Croix',
-        'Pièces' => 3.5
-    ),
-    array(
-        'Habitation' => 'Maison',
-        'Localisation' => 'Yverdon',
-        'Pièces' => 3.5
-    ),
-    array(
-        'Habitation' => 'Appartement',
-        'Localisation' => 'Grandson',
-        'Pièces' => 3.5
-    ),
-    array(
-        'Habitation' => 'Maison',
-        'Localisation' => 'Fribourg',
-        'Pièces' => 3.5
-    ),
-    array(
-        'Habitation' => 'Appartement',
-        'Localisation' => 'Renens',
-        'Pièces' => 3.5
-    )
-);
-
-
 
 function extractFilter($filter){
     $habitation = $filter['habitation'];
@@ -47,16 +13,84 @@ function extractFilter($filter){
 
     $resultFilter = [];
 
-    array_push($resultFilter, $localisation, $habitation);
+    array_push($resultFilter, $habitation, $localisation);
 
     return $resultFilter;
 }
 function filter($filter){
-    $detailFilter = extractFilter($filter);
 
-    return $detailFilter;
+    $colocation = array(
+        array(
+            'Habitation' => 'appartement',
+            'Localisation' => 'Yverdon',
+            'Pièces' => 3.5
+        ),
+        array(
+            'Habitation' => 'appartement',
+            'Localisation' => 'Ste-Croix',
+            'Pièces' => 4
+        ),
+        array(
+            'Habitation' => 'maison',
+            'Localisation' => 'Yverdon',
+            'Pièces' => 3.5
+        ),
+        array(
+            'Habitation' => 'appartement',
+            'Localisation' => 'Grandson',
+            'Pièces' => 2
+        ),
+        array(
+            'Habitation' => 'maison',
+            'Localisation' => 'Fribourg',
+            'Pièces' => 3.5
+        ),
+        array(
+            'Habitation' => 'appartement',
+            'Localisation' => 'Yverdon',
+            'Pièces' => 2.5
+        )
+    );
+
+    $detailFilter = extractFilter($filter);
+    if ($detailFilter[0] == ""){
+        $midDetail = $colocation;
+    }else{
+        $habitation = "Habitation";
+        $midDetail = filterArrayByKeyValueHabitation($colocation, $habitation, $detailFilter[0]);
+    }
+    if ($detailFilter[1] == ""){
+        $fullDetail = $midDetail;
+    }else{
+        $localisation = "Localisation";
+        $fullDetail = filterArrayByKeyValueLocalisation($midDetail, $localisation, $detailFilter[1]);
+    }
+    return $fullDetail;
 }
 
+function filterArrayByKeyValueHabitation($colocation, $index, $detailFilter){
+    if(is_array($colocation)){
+        foreach (array_keys($colocation) as $key){
+            $temp[$key] = $colocation[$key][$index];
+            if ($temp[$key] == $detailFilter){
+                $colocationFilter[$key] = $colocation[$key];
+            }
+        }
+    }
+    return $colocationFilter;
+}
+
+function filterArrayByKeyValueLocalisation($colocation, $index, $detailFilter){
+    if(is_array($colocation)){
+        foreach (array_keys($colocation) as $key){
+            $temp[$key] = $colocation[$key][$index];
+            if ($temp[$key] == $detailFilter){
+                $colocationFilter[$key] = $colocation[$key];
+            }
+        }
+    }
+    return $colocationFilter;
+}
 
 
 
