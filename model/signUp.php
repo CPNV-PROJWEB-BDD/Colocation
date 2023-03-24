6ccc89712ea4b953965b6228894194a834c9a8e7
Ahmed.MUJANOVIC <Ahmed.Mujanovic@cpnv.ch>
2023-03-24 14:01:44 +0100

    password

--- model/signUp.php
+++ model/signUp.php
@@ -5,12 +5,14 @@
 $prenom = $_POST["inputFirstName"];
 $nom = $_POST["inputLastName"];
 $email = $_POST["inputEmailAddress"];
+$motDePasse =$_POST["inputPassword"];
 
 // Création d'un tableau avec les données
 $data = array(
     "prenom" => $prenom,
     "nom" => $nom,
     "email" => $email,
+    "motDePasse" => $motDePasse
 );
 
 // Chargement du contenu du fichier JSON existant
