5c426060fc27d17293fdd2bd29ba411fbd2cdf25
Ahmed.MUJANOVIC <Ahmed.Mujanovic@cpnv.ch>
2023-03-30 16:45:14 +0200

    correction d erreur

--- controller/navigation.php
+++ controller/navigation.php
@@ -5,9 +5,7 @@ function home(){
     require "view/home.php";
 }
 
-function displaySendFormRequest(){
-    require "view/sendFormRequest.php";
-}
+
 
 function displayLogin(){
     require "view/loginForm.php";
